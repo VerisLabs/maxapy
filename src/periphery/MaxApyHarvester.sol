b655f6549147a5fe618a7b970ef1510a69186a9b
fepvenancio <filipeepv@gmail.com>
2024-10-21 11:14:13 +0100

    maxHarvester update

--- src/periphery/MaxApyHarvester.sol
+++ src/periphery/MaxApyHarvester.sol
@@ -4,30 +4,37 @@ pragma solidity ^0.8.19;
 import { OwnableRoles } from "solady/auth/OwnableRoles.sol";
 import { IStrategy } from "src/interfaces/IStrategy.sol";
 import { IMaxApyVault } from "src/interfaces/IMaxApyVault.sol";
+import { StrategyData } from "../helpers/VaultTypes.sol";
 
-/// @title MaxApyHarvester
-/// @dev This is an internal contract to call harvest in an atomic way.
+/**
+ * @title MaxHarvester
+ * @dev This is an internal contract to call harvest in an atomic way. 
+ */
 contract MaxApyHarvester is OwnableRoles {
-    ////////////////////////////////////////////////////////////////
-    ///                        ERRORS                            ///
-    ////////////////////////////////////////////////////////////////
-    error HarvestFailed();
-    error NotOwner();
+
+    /*//////////////////////////////////////////////////////////////
+                             ERRORS
+    //////////////////////////////////////////////////////////////*/
+    error AddStrategyFailed();
     error CantReceiveETH();
     error Fallback();
+    error HarvestFailed();
+    error NotOwner();
+    error MaximumStrategiesReached();
 
-    ////////////////////////////////////////////////////////////////
-    ///                        STRUCTS                           ///
-    ////////////////////////////////////////////////////////////////
-    /// @dev Params for harvesting
-    struct HarvestData {
-        address strategyAddress;
-        uint256 minExpectedBalance;
-        uint256 minOutputAfterInvestment;
-        uint256 deadline;
-    }
+    IStrategy strategy;
 
-    /// @dev Params for allocation
+    /*//////////////////////////////////////////////////////////////
+                              STRUCTS
+    //////////////////////////////////////////////////////////////*/
+    /**
+     * @dev Struct to encapsulate information about the strategy to add.
+     * @param strategyAddress The address of the strategy.
+     * @param strategyDebtRatio The debt ratio for the strategy.
+     * @param strategyMaxDebtPerHarvest The maximum debt per harvest for the strategy.
+     * @param strategyMinDebtPerHarvest The minimum debt per harvest for the strategy.
+     * @param strategyPerformanceFee The performance fee for the strategy.
+     */
     struct AllocationData {
         address strategyAddress;
         uint256 debtRatio;
@@ -36,28 +43,49 @@ contract MaxApyHarvester is OwnableRoles {
         uint256 performanceFee;
     }
 
+    /**
+     * @dev Struct to encapsulate information about an individual harvest.
+     * @param strategyAddress The address of the strategy to harvest from.
+     * @param minExpectedBalance The minimum expected balance after the harvest.
+     * @param minOutputAfterInvestment The minimum output after the investment.
+     * @param deadline The deadline for the harvest operation.
+     */
+    struct HarvestData {
+        address strategyAddress;
+        uint256 minExpectedBalance;
+        uint256 minOutputAfterInvestment;
+        uint256 deadline;
+    }
+
     ////////////////////////////////////////////////////////////////
     ///                        CONSTANTS                         ///
     ////////////////////////////////////////////////////////////////
+    // ROLES
     uint256 public constant ADMIN_ROLE = _ROLE_0;
     uint256 public constant KEEPER_ROLE = _ROLE_1;
+    // ACTORS
     address public constant DEFAULT_HARVESTER = address(0);
 
     ////////////////////////////////////////////////////////////////
     ///                       MODIFIERS                          ///
     ////////////////////////////////////////////////////////////////
+    /**
+     * @dev Modifier to check if the caller has the required roles.
+     * @param roles The roles to check.
+     */
     modifier checkRoles(uint256 roles) {
         _checkRoles(roles);
         _;
     }
 
-    ////////////////////////////////////////////////////////////////
-    ///                     INITIALIZATION                       ///
-    ////////////////////////////////////////////////////////////////
-
-    /// @dev Constructor to set the initial state of the contract.
-    /// @param admin Contract admin
-    /// @param keepers The addresses that will be added to a keeper role
+    /*//////////////////////////////////////////////////////////////
+                          INITIALIZATION
+    //////////////////////////////////////////////////////////////*/
+    /**
+     * @dev Constructor to set the initial state of the contract.
+     * @param admin The address of the admin.
+     * @param keepers An array of addresses for the keepers that will call the contract functions.
+     */
     constructor(address admin, address[] memory keepers) {
         // loop to add the keepers to a mapping
         _initializeOwner(admin);
@@ -65,8 +93,8 @@ contract MaxApyHarvester is OwnableRoles {
 
         uint256 length = keepers.length;
 
-        // Iterate through each keeper in the array in order to grant roles.
-        for (uint256 i = 0; i < length;) {
+        // Iterate through each Keeper in the array in order to grant roles.
+        for (uint256 i = 0; i < length; ) {
             _grantRoles(keepers[i], KEEPER_ROLE);
 
             unchecked {
@@ -75,67 +103,99 @@ contract MaxApyHarvester is OwnableRoles {
         }
     }
 
-    ////////////////////////////////////////////////////////////////
-    ///                     FALLBACK & RECEIVE                   ///
-    ////////////////////////////////////////////////////////////////
-    /// @dev Explicitly reject any raw call
+    /*//////////////////////////////////////////////////////////////
+                    Fallback and Receive Functions
+    //////////////////////////////////////////////////////////////*/
+    /**
+     * @dev Fallback function to reject any Ether sent to the contract.
+     */
     fallback() external payable {
         revert Fallback();
     }
 
-    /// @dev Explicitly reject any Ether transfered to the contract
+    /**
+     * @dev Receive function to reject any Ether transferred to the contract.
+     */
     receive() external payable {
         revert CantReceiveETH();
     }
 
-    ////////////////////////////////////////////////////////////////
-    ///                     LOGIC                                ///
-    ////////////////////////////////////////////////////////////////
-    /// @notice Orchestrates a batch harvest for the MaxApy protocol.
-    /// @param harvests An array of strategy harvests
-    function batchHarvests(HarvestData[] calldata harvests) public checkRoles(KEEPER_ROLE) {
-        uint256 length = harvests.length;
+    /*//////////////////////////////////////////////////////////////
+                          LOGIC
+    //////////////////////////////////////////////////////////////*/
+    /**
+     * @dev Orchestrates a batch add strategy for the maxapy protocol.
+     * @param vault The MaxApyVault contract instance.
+     * @param strategies An array of strategy values to add to the vault.
+     */
+    function batchAllocate(
+        IMaxApyVault vault,
+        AllocationData[] calldata strategies
+    ) public checkRoles(KEEPER_ROLE) {
+        uint256 length = strategies.length;
 
-        // Iterate through each strategy in the array in order to call the harvest.
-        for (uint256 i = 0; i < length;) {
-            address strategyAddress = harvests[i].strategyAddress;
-            uint256 minExpectedBalance = harvests[i].minExpectedBalance;
-            uint256 minOutputAfterInvestment = harvests[i].minOutputAfterInvestment;
-            uint256 deadline = harvests[i].deadline;
+        AllocationData calldata stratData;
+        StrategyData memory isStratActive;
 
-            IStrategy(strategyAddress).harvest(
-                minExpectedBalance, minOutputAfterInvestment, DEFAULT_HARVESTER, deadline
-            );
-
-            // Use unchecked block to bypass overflow checks for efficiency.
+        // Iterate through each strategy in the array in order to add the strategy .
+        for (uint i = 0; i < length; ) {
+            stratData = strategies[i];
+            isStratActive = vault.strategies(stratData.strategyAddress);
+            if (isStratActive.strategyActivation != 0) {
+                vault.updateStrategyData(
+                    stratData.strategyAddress,
+                    stratData.debtRatio,
+                    stratData.maxDebtPerHarvest,
+                    stratData.minDebtPerHarvest,
+                    stratData.performanceFee
+                );
+            } else {
+                vault.addStrategy(
+                    stratData.strategyAddress,
+                    stratData.debtRatio,
+                    stratData.maxDebtPerHarvest,
+                    stratData.minDebtPerHarvest,
+                    stratData.performanceFee
+                );
+            }
+            
             unchecked {
                 i++;
             }
         }
     }
 
-    /// @notice Orchestrates a batch allocation for the MaxApy protocol.
-    /// @param allocations An array of strategy allocations
-    function batchAllocate(
+    /**
+     * @dev Orchestrates a batch remove strategy for the maxapy protocol.
+     * @param vault The MaxApyVault contract instance.
+     * @param harvests An array of harvest data for strategies to remove from the vault.
+     */
+    function batchHarvest(
         IMaxApyVault vault,
-        AllocationData[] calldata allocations
-    )
-        public
-        checkRoles(KEEPER_ROLE)
-    {
-        uint256 length = allocations.length;
+        HarvestData[] calldata harvests
+    ) public checkRoles(KEEPER_ROLE) {
+        uint256 length = harvests.length;
 
-        // Iterate through each strategy in the array in order to call the allocate.
-        for (uint256 i = 0; i < length;) {
-            address strategyAddress = allocations[i].strategyAddress;
-            uint256 debtRatio = allocations[i].debtRatio;
-            uint256 maxDebtPerHarvest = allocations[i].maxDebtPerHarvest;
-            uint256 minDebtPerHarvest = allocations[i].minDebtPerHarvest;
-            uint256 performanceFee = allocations[i].performanceFee;
+        // Iterate through each strategy in the array in order to call the harvest.
+        StrategyData memory strategyData;
 
-            vault.updateStrategyData(strategyAddress, debtRatio, maxDebtPerHarvest, minDebtPerHarvest, performanceFee);
+        for (uint i = 0; i < length; ) {
+            address strategyAddress = harvests[i].strategyAddress;
 
-            // Use unchecked block to bypass overflow checks for efficiency.
+            strategyData = vault.strategies(strategyAddress);
+
+            strategy = IStrategy(strategyAddress);
+            strategy.harvest(
+                    harvests[i].minExpectedBalance,
+                    harvests[i].minOutputAfterInvestment,
+                    DEFAULT_HARVESTER,
+                    harvests[i].deadline
+                );
+
+            if (strategyData.strategyDebtRatio == 0) {
+                vault.exitStrategy(strategyAddress);
+            }
+
             unchecked {
                 i++;
             }
@@ -151,7 +211,7 @@ contract MaxApyHarvester is OwnableRoles {
         returns (bool)
     {
         batchAllocate(vault, allocations);
-        batchHarvests(harvests);
+        batchHarvest(vault, harvests);
         return true;
     }
 }
