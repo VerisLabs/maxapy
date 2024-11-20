06db460e347ad0546e891655990c60808211ae4e
Shubhangi <sdargarh26@gmail.com>
2024-11-20 17:01:50 +0530

    added preview fuzz tests

--- test/unit/strategies/CompoundV3USDTStrategy.t.sol
+++ test/unit/strategies/CompoundV3USDTStrategy.t.sol
@@ -538,4 +538,79 @@ contract CompoundV3USDTStrategyTest is BaseTest, StrategyEvents {
 
         strategy.harvest(expectedBalance, outputAfterInvestment, address(0), block.timestamp);
     }
+
+    function testCompoundV3USDT__PreviewLiquidate__FUZZY(uint256 amount) public {
+        vm.assume(amount > _1_USDC && amount < 100000 * _1_USDC);
+        deal(USDC_MAINNET, users.alice, amount);
+
+        vault.addStrategy(address(strategy), 4000, type(uint72).max, 0, 0);
+        vault.deposit(amount, users.alice);
+        vm.startPrank(users.keeper);
+        strategy.harvest(0, 0, address(0), block.timestamp);
+        vm.stopPrank();
+        uint256 expected = strategy.previewLiquidate(amount / 3);
+        vm.startPrank(address(vault));
+        uint256 loss = strategy.liquidate(amount / 3);
+
+        assertLe(expected, amount / 3 - loss);
+
+    }
+
+    function testCompoundV3USDT__PreviewLiquidateExact__FUZZY(uint256 amount) public {
+        vm.assume(amount > _1_USDC && amount < 100000 * _1_USDC);
+        deal(USDC_MAINNET, users.alice, amount);
+
+        vault.addStrategy(address(strategy), 4000, type(uint72).max, 0, 0);
+        vault.deposit(amount, users.alice);
+        vm.startPrank(users.keeper);
+        strategy.harvest(0, 0, address(0), block.timestamp);
+        vm.stopPrank();
+        uint256 requestedAmount = strategy.previewLiquidateExact(amount / 3);
+        vm.startPrank(address(vault));
+        uint256 balanceBefore = IERC20(USDC_MAINNET).balanceOf(address(vault));
+        strategy.liquidateExact(amount / 3);
+        uint256 withdrawn = IERC20(USDC_MAINNET).balanceOf(address(vault)) - balanceBefore;
+        // withdraw exactly what requested
+        assertEq(withdrawn, amount / 3);
+        // losses are equal or fewer than expected
+        assertLe(withdrawn - amount / 3, requestedAmount - amount / 3);
+    }
+
+    function testCompoundV3USDT__maxLiquidateExact__FUZZY(uint256 amount) public {
+        vm.assume(amount > _1_USDC && amount < 100000 * _1_USDC);
+        deal(USDC_MAINNET, users.alice, amount);
+
+        vault.addStrategy(address(strategy), 9000, type(uint72).max, 0, 0);
+        vault.deposit(amount, users.alice);
+        vm.startPrank(users.keeper);
+        strategy.harvest(0, 0, address(0), block.timestamp);
+        vm.stopPrank();
+        uint256 maxLiquidateExact = strategy.maxLiquidateExact();
+        uint256 balanceBefore = IERC20(USDC_MAINNET).balanceOf(address(vault));
+        uint256 requestedAmount = strategy.previewLiquidateExact(maxLiquidateExact);
+        vm.startPrank(address(vault));
+        uint256 losses = strategy.liquidateExact(maxLiquidateExact);
+        uint256 withdrawn = IERC20(USDC_MAINNET).balanceOf(address(vault)) - balanceBefore;
+        // withdraw exactly what requested
+        assertEq(withdrawn, maxLiquidateExact);
+        // losses are equal or fewer than expected
+        assertLe(losses, requestedAmount - maxLiquidateExact);
+    }
+
+    function testCompoundV3USDT__MaxLiquidate__FUZZY(uint256 amount) public {
+        vm.assume(amount > _1_USDC && amount < 100000 * _1_USDC);
+        deal(USDC_MAINNET, users.alice, amount);
+        
+        vault.addStrategy(address(strategy), 9000, type(uint72).max, 0, 0);
+        vault.deposit(amount, users.alice);
+        vm.startPrank(users.keeper);
+        strategy.harvest(0, 0, address(0), block.timestamp);
+        vm.stopPrank();
+        uint256 maxWithdraw = strategy.maxLiquidate();
+        uint256 balanceBefore = IERC20(USDC_MAINNET).balanceOf(address(vault));
+        vm.startPrank(address(vault));
+        strategy.liquidate(maxWithdraw);
+        uint256 withdrawn = IERC20(USDC_MAINNET).balanceOf(address(vault)) - balanceBefore;
+        assertLe(withdrawn, maxWithdraw);
+    }
 }
